f749c81dd68cad128e95d460277e9422b3a51329
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-03-25 00:03:46 -0400

    Atualização de nomesde pastas e arquivos do repositório

---
+++ FUNDAMENTALS/BLOCK_04/DIA_01/scripts.js
@@ -0,0 +1,20 @@
+let resultado = "aprovada";
+
+switch (resultado) {
+  case "aprovada":
+      console.log("Aprovada(o)");
+      break;
+
+  case "lista":
+      console.log("Você esta na lista de espera");
+      break;
+
+  case "reprovada":
+      console.log("Reprovada(o)");
+      break;
+
+  default:
+      console.log("não se aplica");
+        break;
+}
+
