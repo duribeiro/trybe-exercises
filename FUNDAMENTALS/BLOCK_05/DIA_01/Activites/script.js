207fe2cd430b0449456271290642fec094f2f028
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-04-30 00:58:19 -0400

    Atividade do getElementById

--- FUNDAMENTALS/BLOCK_05/DIA_01/Activites/script.js
+++ FUNDAMENTALS/BLOCK_05/DIA_01/Activites/script.js
@@ -1,2 +1,9 @@
 var paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
+
+let newTitle = document.getElementById('page-title')
+newTitle.innerText = 'Título dinâmico';
+console.log(newTitle);
+
+let newParagraph = document.getElementById('second-paragraph')
+newParagraph.innerText = 'Aqui está meu parágrafo escrito de forma dinâmica com JS';
