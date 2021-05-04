84e0ec77aceea8e279eaa15a4c0d312912d86f48
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-04 11:58:22 -0400

    Arquivos da atividade de aprendizagem do boloco 5 dia 3

--- FUNDAMENTALS/BLOCK_05/DIA_01/Activites/script.js
+++ FUNDAMENTALS/BLOCK_05/DIA_01/Activites/script.js
@@ -3,7 +3,17 @@ paragraph.style.color = "red";
 
 let newTitle = document.getElementById('page-title')
 newTitle.innerText = 'Título dinâmico';
-console.log(newTitle);
 
 let newParagraph = document.getElementById('second-paragraph')
 newParagraph.innerText = 'Aqui está meu parágrafo escrito de forma dinâmica com JS';
+// paragraphs[0].style.backgroundColor = 'red'
+
+let paragraphs = document.getElementsByClassName('paragraph')
+
+// window.getComputedStyle(paragraphs[0]).backgroundColor = 'rgb(29, 100, 29)'
+
+// window.getComputedStyle(paragraphs[0]).getPropertyValue('background-color') = 'rgb(029, 100, 029)'
+
+console.log(window.getComputedStyle(paragraphs[0]).getPropertyValue('background-color'))
+
+console.log(window.getComputedStyle(paragraphs[0]).backgroundColor)
