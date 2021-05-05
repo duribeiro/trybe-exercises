ca93eed0f50f982e6fd8f4ebd5a4be2aa1857aa9
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-04 21:54:00 -0400

    adiciona um comentario sobre a função hover

--- FUNDAMENTALS/BLOCK_05/DIA_03/main.js
+++ FUNDAMENTALS/BLOCK_05/DIA_03/main.js
@@ -63,6 +63,8 @@ function redirectPage() {
 }
 redirectPage();
 
+// Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
 function hoverH3() {
   myWebpage.addEventListener('mouseenter', (event) => {
     myWebpage.style.color = '#2fc18c'
