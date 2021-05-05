94153ab40ac67da94b936174487e94de7d625d30
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-05 11:50:23 -0400

    Desafio 1 completo

---
+++ FUNDAMENTALS/BLOCK_05/DIA_02/Activites/script.js
@@ -0,0 +1,4 @@
+// 1. Acesse o elemento elementoOndeVoceEsta .
+const elementHere = document.getElementById('elementoOndeVoceEsta')
+
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
