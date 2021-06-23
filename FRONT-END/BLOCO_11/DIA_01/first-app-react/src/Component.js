d0179c20d4d54e4b6350307dd7a5785d9bd5bb14
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-06-23 00:25:20 -0400

    Criando o primeiro componente react

---
+++ FRONT-END/BLOCO_11/DIA_01/first-app-react/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class myComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Eduardo Ribeiro</h1>
+        <p>Sou apaixonado por tecnologia e meu maior sonho <br></br> é usar a tecnologia e educação para transformar a vida das pessoas.</p>
+      </div>
+    )
+  }
+}
+
+export default myComponent
