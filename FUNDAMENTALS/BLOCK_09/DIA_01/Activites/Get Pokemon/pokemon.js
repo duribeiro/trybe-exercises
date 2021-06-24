e86a889f76525c25ec11dce8f392fe3b70492f9a
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-06-24 13:50:23 -0400

    Recebendo os dados da api

--- FUNDAMENTALS/BLOCK_09/DIA_01/Activites/Get Pokemon/pokemon.js
+++ FUNDAMENTALS/BLOCK_09/DIA_01/Activites/Get Pokemon/pokemon.js
@@ -10,8 +10,11 @@ const appendPokemon = (pokemon) => {
 const fetchPokemon = () => {
   // appendPokemon('skirtle')
   // response não é a resposta final é apenas o "indice" do "livro maior" que é a promisse, são informações sobre o statos da requisão.
+  // É necessário chamar a função json() apartir de do response, o retorno dessa função é a promisse contendo os dado. Só então é que podemos aplicar o .then e ver os dados individuais, nesse caso o pokemon pesquisado
   const api = fetch('https://pokeapi.co/api/v2/pokemon/squirtle').then((response) => {
-    console.log(response);
+    response.json().then((pokemon) => {
+      console.log(pokemon);
+    })
   } );
   console.log(api);
 };
