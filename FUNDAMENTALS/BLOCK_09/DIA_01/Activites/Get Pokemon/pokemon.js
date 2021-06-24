ffae5fac1e71a866be745d913ee839beb282aaec
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-06-24 13:45:52 -0400

    Abstraçoes sobre o uso das promisses

--- FUNDAMENTALS/BLOCK_09/DIA_01/Activites/Get Pokemon/pokemon.js
+++ FUNDAMENTALS/BLOCK_09/DIA_01/Activites/Get Pokemon/pokemon.js
@@ -1,8 +1,22 @@
-const fetchPokemon = () => {
+const appendPokemon = (pokemon) => {
+  const ul = document.querySelector('ul');
+  const li = document.createElement('li');
 
-}
+  li.innerHTML = pokemon;
+
+  ul.appendChild(li);
+};
+
+const fetchPokemon = () => {
+  // appendPokemon('skirtle')
+  // response não é a resposta final é apenas o "indice" do "livro maior" que é a promisse, são informações sobre o statos da requisão.
+  const api = fetch('https://pokeapi.co/api/v2/pokemon/squirtle').then((response) => {
+    console.log(response);
+  } );
+  console.log(api);
+};
 
 // window.onload vai chamar o fetchPokemon depois que tudo tiver carregado no meu html
 window.onload = () => {
   fetchPokemon();
-}
+};
