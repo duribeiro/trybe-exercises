c01b21e3501509c08800a55e9b191ab8cee4ade1
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-06-27 21:15:16 -0400

    criando uma funçao semi automatica para fazer fetch na api dos pokemons

--- FUNDAMENTALS/BLOCK_09/DIA_02/Activites/Get Pokemon/pokemon.js
+++ FUNDAMENTALS/BLOCK_09/DIA_02/Activites/Get Pokemon/pokemon.js
@@ -13,23 +13,33 @@ const appendPokemon = (pokemon) => {
   console.log(pokemon.sprites.front_default);
 };
 
-const fetchPokemon = () => {
-  // appendPokemon('skirtle')
-  // response não é a resposta final é apenas o "indice" do "livro maior" que é a promisse, são informações sobre o statos da requisão.
-  // É necessário chamar a função json() apartir de do response, o retorno dessa função é a promisse contendo os dado. Só então é que podemos aplicar o .then e ver os dados individuais, nesse caso o pokemon pesquisado
-  fetch('https://pokeapi.co/api/v2/pokemon/squirtle').then((response) => {
-    response.json().then((pokemon) => {
+const getPokemon = (namePokemon) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`).then((response) => {
+    response.json().then((pokemon)=> {
       appendPokemon(pokemon);
-
-      fetch('https://pokeapi.co/api/v2/pokemon/pikachu').then((response) => {
-        response.json().then((pokemon) => {
-          appendPokemon(pokemon);
-
-        })
-      });
-    })
+    });
   });
 };
+
+getPokemon('pikachu')
+
+// const fetchPokemon = () => {
+//   // appendPokemon('skirtle')
+//   // response não é a resposta final é apenas o "indice" do "livro maior" que é a promisse, são informações sobre o statos da requisão.
+//   // É necessário chamar a função json() apartir de do response, o retorno dessa função é a promisse contendo os dado. Só então é que podemos aplicar o .then e ver os dados individuais, nesse caso o pokemon pesquisado
+//   fetch('https://pokeapi.co/api/v2/pokemon/squirtle').then((response) => {
+//     response.json().then((pokemon) => {
+//       appendPokemon(pokemon);
+
+//       fetch('https://pokeapi.co/api/v2/pokemon/pikachu').then((response) => {
+//         response.json().then((pokemon) => {
+//           appendPokemon(pokemon);
+
+//         })
+//       });
+//     })
+//   });
+// };
 
 // window.onload vai chamar o fetchPokemon depois que tudo tiver carregado no meu html
 window.onload = () => {
