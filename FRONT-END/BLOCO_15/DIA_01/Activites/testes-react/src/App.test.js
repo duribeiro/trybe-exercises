aca3ba80f5a4566c6d72794d09df110827e5bf88
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-16 15:25:29 -0400

    Adiociona arquivos do bloco 15.1

---
+++ FRONT-END/BLOCO_15/DIA_01/Activites/testes-react/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Verificando se existe o campo Email.', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  const { getByTestId } = render(<App />);
+  const btn = getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+  expect(btn.type).toBe('button');
+  expect(btn).toHaveValue('Enviar');
+});
+
+test('Verificando se existe um botão', () => {
+  const { getAllByRole } = render(<App />);
+  const buttons = getAllByRole('button');
+  expect(buttons.length).toBe(2);;
+});
