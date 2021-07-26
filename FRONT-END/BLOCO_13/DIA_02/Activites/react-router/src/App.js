01fbd463b488f5e283c2cf8af1af1052a09b736d
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-26 10:43:36 -0400

    Passando props pelo router

--- FRONT-END/BLOCO_13/DIA_02/Activites/react-router/src/App.js
+++ FRONT-END/BLOCO_13/DIA_02/Activites/react-router/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class App extends Component {
   render() {
-    const ComponentPai = (props) => {
-      return (
-        <div>
-          {props.children}
-        </div>
-      )
-    }
-
     return (
-      <div className="App App-header">
-        <ComponentPai>
-          <p>Estou no componente Pai</p>
-        </ComponentPai>
+      <div className='App App-header'>
+        <BrowserRouter>
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/profile'>Profile</Link>
+          </nav>
+          <Route exact path='/' component={Home} />
+          <Route path='/About' component={About} />
+          <Route
+            path='/Profile'
+            render={(props) => <Profile {...props} name="Eduardo" />} />
+        </BrowserRouter>
       </div>
     );
   }
