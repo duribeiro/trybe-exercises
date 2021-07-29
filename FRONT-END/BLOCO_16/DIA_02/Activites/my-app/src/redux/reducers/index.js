f5f5654a80f2678b7a9043e63746c47bac4f718f
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-28 23:47:21 -0400

    Adiona arquivos do bloco 16

---
+++ FRONT-END/BLOCO_16/DIA_02/Activites/my-app/src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+ const store = createStore((state = {}) => (state), composeWithDevTools())
+
+ export default store;
