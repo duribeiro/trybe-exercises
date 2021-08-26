8977cb742c7d3e94790a50176fd0fb8038f23e27
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-08-26 16:14:52 -0400

    Testa o comportamento da aplicação de apresentar o email na tela

--- FRONT-END/BLOCO_15/DIA_01/Activites/testes-react/src/App.js
+++ FRONT-END/BLOCO_15/DIA_01/Activites/testes-react/src/App.js
@@ -10,17 +10,36 @@ export default class App extends Component {
     };
   }
 
-  
+  changeEmail(email) {
+    this.setState({ email: email })
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' })
+  }
 
   render() {
+    const { email, saveEmail } = this.state;
     return (
       <div className="App">
         <label htmlFor="id-email">
           Email
         </label>
-        <input id="id-email" type="email" />
-        <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
+        <input 
+          id="id-email"
+          type="email"
+          value={ email }
+          onChange={ (e) => this.changeEmail(e.target.value) }
+        />
+        <input
+          id="btn-send"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={ () => this.changeSaveEmail(email)}
+        />
         <input id="btn-back" type="button" value="Voltar" />
+        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
       </div>
     );
   }
--- FRONT-END/BLOCO_15/DIA_01/Activites/testes-react/src/App.test.js
+++ FRONT-END/BLOCO_15/DIA_01/Activites/testes-react/src/App.test.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('Verificando se existe o campo Email.', () => {
-  const { getByLabelText } = render(<App />);
-  const imputEmail = getByLabelText('Email');
-  expect(imputEmail).toBeInTheDocument();
-  expect(imputEmail.type).toBe('email')
-});
+describe('Verificando os campos na tela', () => {
+  it('Verificando se existe o campo Email.', () => {
+    const { getByLabelText } = render(<App />);
+    const imputEmail = getByLabelText('Email');
+    expect(imputEmail).toBeInTheDocument();
+    expect(imputEmail.type).toBe('email')
+  });
+  
+  it('Verificando se existe um botão de enviar', () => {
+    // acessar o elemento
+    const { getByTestId } = render(<App />);
+    const btn = getByTestId('id-send')
+    // interagir com o elemento
+    // testar o elemento
+    expect(btn).toBeInTheDocument();
+    expect(btn.type).toBe('button')
+    expect(btn).toHaveValue('Enviar')
+  });
+  
+  it('Verificando se existe um botão', () => {
+    const { getAllByRole } = render(<App />);
+    const buttons = getAllByRole('button');
+    expect(buttons.length).toBe(2);;
+  });
 
-test('Verificando se existe um botão de enviar', () => {
-  // acessar o elemento
-  const { getByTestId } = render(<App />);
-  const btn = getByTestId('id-send')
-  // interagir com o elemento
-  // testar o elemento
-  expect(btn).toBeInTheDocument();
-  expect(btn.type).toBe('button')
-  expect(btn).toHaveValue('Enviar')
-});
+  it('Verifica se o botão e o campo email estão funcionando.', () => {
+    // acessar os elementos
+    const { getByTestId, getByLabelText } = render(<App />);
+    
+    const EMAIL_USER = 'email@email.com';
 
-test('Verificando se existe um botão', () => {
-  const { getAllByRole } = render(<App />);
-  const buttons = getAllByRole('button');
-  expect(buttons.length).toBe(2);;
-});
+    const textEmail = getByTestId('id-email-user');
+    expect(textEmail).toBeInTheDocument();
+    expect(textEmail).toHaveTextContent('Valor:')
+
+    const btnSend = getByTestId('id-send');
+    const inputEmail = getByLabelText('Email')
+    fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+    fireEvent.click(btnSend);
+
+    expect(inputEmail).toHaveValue('');
+    expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+  })
+})
