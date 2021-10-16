93c52c4593771f67a1f79a7b78877b3a9f6963b6
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-16 17:46:21 -0400

    REDUX DIA 2 - corrige o myReducer

--- FRONT-END/REDUX/DIA_02/Activites/my-app-redux/src/reducers/myReducer.js
+++ FRONT-END/REDUX/DIA_02/Activites/my-app-redux/src/reducers/myReducer.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
 
 function myReducer (state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'NEW_ACTION';
+    case 'NEW_ACTION':
       return { state: action.state };
     default:
       return state;
