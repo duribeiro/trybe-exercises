4d7516ff497b15e46b76346d575ef463c575ab47
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-19 22:56:04 -0400

    Configura as propriedades do mapa

--- FRONT-END/REDUX/DIA_04/Activites/iss-location/src/components/ISSLocation.js
+++ FRONT-END/REDUX/DIA_04/Activites/iss-location/src/components/ISSLocation.js
@@ -5,7 +5,14 @@ export default class ISSLocation extends Component {
   render() {
     return (
       <div>
-        <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+        <Map
+          center={[0,0]}
+          defaultWidth={450}
+          height={450}
+          minZoom={1.5}
+          maxZoom={8}
+          zoom={1.5}
+        >
           <Marker width={50} anchor={[50.879, 4.6997]} />
         </Map>
       </div>
