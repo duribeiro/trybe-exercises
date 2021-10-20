bc03d71c19f6e2f534768f354e4fb73ac99ea279
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-20 00:08:14 -0400

    Configura a primeira action

---
+++ FRONT-END/REDUX/DIA_04/Activites/iss-location/src/actions/index.js
@@ -0,0 +1,9 @@
+export const RECEIVE_ISS_LOCATION = 'RECEIVE_ISS_LOCATION';
+
+export const receiveIssLocation = () => {
+  return {
+    type: RECEIVE_ISS_LOCATION,
+    latitude: 2.81954,
+    longitude: -60.6714,
+  }
+}
--- FRONT-END/REDUX/DIA_04/Activites/iss-location/src/reducers/ISSLocation.js
+++ FRONT-END/REDUX/DIA_04/Activites/iss-location/src/reducers/ISSLocation.js
@@ -1,5 +1,21 @@
-const ISSLocation = (state = {}, action) => {
-  return state;
+import { RECEIVE_ISS_LOCATION } from "../actions";
+
+const INITIAL_STATE = {
+  latitude: null,
+  longitude: null,
+}
+
+const ISSLocation = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case RECEIVE_ISS_LOCATION:
+      return {
+        ...state,
+        latitude: action.latitude,
+        longitude: action.longitude,
+      }
+    default:
+      return state;
+  }
 }
 
 export default ISSLocation;
