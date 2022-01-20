7398b819742dd3e2fed1ad944003e93ef1c13904
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-01-20 12:44:18 -0400

    Cria o arqvivo Author para fazer quries no db

---
+++ BACK-END/NODE-REST/DIA_01/model-example/models/Author.js
@@ -0,0 +1,13 @@
+const connection = require('../connection');
+
+// Busca todas as peessoas autoras do banco
+const getAll = async () => {
+  const [authors] = await connection.execute(
+    'SELECT id, first_name, middle_name, last_name FROM model_example.authors;'
+  );
+  return authors;
+};
+
+module.exports = {
+  getAll,
+}
