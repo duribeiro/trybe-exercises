2b1318ecebda384bf452079b2c5db2d2eaa00a79
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-01-20 19:57:23 -0400

    Implementa função que busca autor por id

--- BACK-END/NODE-REST/DIA_01/model-example/models/Author.js
+++ BACK-END/NODE-REST/DIA_01/model-example/models/Author.js
@@ -36,13 +36,13 @@ const findById = async (id) => {
   // Depois, ao executá-la, informamos um array com o id para o método `execute`.
   // O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
 
-  const query = 'SELECT first_name, middle_name, last_name FROM model_example.authors;';
-  const [authorsData] = await connection.execute(query, [id]);
+  const query = 'SELECT first_name, middle_name, last_name FROM model_example.authors WHERE id = ?;';
+  const [authorData] = await connection.execute(query, [id]);
 
-  if (authorsData.length === 0) return null;
+  if (authorData.length === 0) return null;
 
   // Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
-  const { first_name, middle_name, last_name } = authorsData.map(serialize)[0];
+  const { firstName, middleName, lastName } = authorData.map(serialize)[0];
 
   return getNewAuthor({
     id,
