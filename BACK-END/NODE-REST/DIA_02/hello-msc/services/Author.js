ea618a902ff200541da30e77d0bca6d57ef1e482
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-01-21 18:48:08 -0400

    Refatora o metodo author na camasa service

--- BACK-END/NODE-REST/DIA_02/hello-msc/services/Author.js
+++ BACK-END/NODE-REST/DIA_02/hello-msc/services/Author.js
@@ -1,13 +1,12 @@
 // services/Authors.js
 
-const Author = require('../models/Authors');
+const Author = require('../models/Author');
 
+// Cria uma string com o nome completo da pessoa autora
 const getNewAuthor = (authorData) => {
   const { id, firstName, middleName, lastName } = authorData;
 
-  const fullName = [firstName, middleName, lastName]
-    .filter((name) => name)
-    .join(' ');
+  const fullName = [firstName, middleName, lastName].filter((name) => name).join(' ');
 
   return {
     id,
@@ -24,13 +23,13 @@ const isValid = (firstName, middleName, lastName) => {
   if (middleName && typeof middleName !== 'string') return false;
 
   return true;
-  }
+};
 
 const getAll = async () => {
   const authors = await Author.getAll();
 
   return authors.map(getNewAuthor);
-}
+};
 
 const findById = async (id) => {
   const author = await Author.findById(id);
@@ -45,12 +44,18 @@ const createAuthor = async (firstName, middleName, lastName) => {
 
   if (!validAuthor) return false;
 
-  await Author.createAuthor(firstName, middleName, lastName);
+  const { id } = await Author.createAuthor(firstName, middleName, lastName);
 
-  return true;
+  return getNewAuthor({
+    id,
+    firstName,
+    middleName,
+    lastName,
+  });
 };
 
 module.exports = {
   getAll,
   findById,
-}
+  createAuthor,
+};
