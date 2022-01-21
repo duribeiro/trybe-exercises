8e4de99fd38da8d597b810b26bba0d0a7cb6d69e
duribeiro <eduardo.duduribeiro1@gmail.com>
2022-01-20 22:30:15 -0400

    Instala o body-parser e cria a rota de POST para o author

--- BACK-END/NODE-REST/DIA_01/model-example/index.js
+++ BACK-END/NODE-REST/DIA_01/model-example/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Author = require('./models/Author');
 const Book = require('./models/Book');
 
 const app = express();
+
+const bodyParser = require('body-parser');
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
@@ -24,6 +27,18 @@ app.get('/authors/:id', async (req, res) => {
   if (!author) return res.status(404).json({ message: 'not found' });
 
   res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
 });
 
 const PORT = process.env.PORT || 3000;
